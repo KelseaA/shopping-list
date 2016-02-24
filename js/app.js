bb512e643adc798fa3a84ccb54672d418f14d430
Kelsea <andersonkelsea@gmail.com>
2016-02-24 18:18:33 -0500

    Shopping List Finished

--- js/app.js
+++ js/app.js
@@ -5,11 +5,27 @@ $(document).ready(function() {
 			$('.add').click();
 		};
 	});	
-	$(".add").click(function(){
-		var toAdd = $("input[name=enterItem]").val();
-		$("ul").append('<div class="list-item"><i class="fa fa-square-o"></i>' + '<li>' + toAdd + '</li>' + '<i class="fa fa-times-circle delete"></i></div>');
+	// adds list items
+	$('.add').click(function(){
+		var toAdd = $('input[name=enterItem]').val();
+		if($('#enter-item').val() == 0){
+			alert("Please enter an item to add to the list");
+		}
+		else {
+		$('ul').append('<div class="list-item"><i id="uncheck" class="fa fa-square-o"></i>' + '<li>' + toAdd + '</li>' + '<i class="fa fa-times-circle delete"></i></div>');
+		// resets text box
+		$('input:text').val('');
+		};
 	});
-	$(document).on("click", ".delete", function(){
-		$(this).closest(".list-item").remove();
+	// removes list items
+	$(document).on('click', '.delete', function(){
+		$(this).closest('.list-item').remove();
+	});
+	// toggles check box
+	$(document).on('click', '#uncheck', function(){
+		$(this).toggleClass("fa-square-o fa-check-square");
 	});
 });
+
+
+
